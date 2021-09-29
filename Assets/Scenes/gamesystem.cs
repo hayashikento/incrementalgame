60362607b30ae1beecb9551f2d0fef0f8d1cd8be
林健人 <ion02181128@gmail.com>
2021-09-29 17:20:09 +0900

    二回目

--- Assets/Scenes/gamesystem.cs
+++ Assets/Scenes/gamesystem.cs
@@ -5,18 +5,31 @@ using UnityEngine.UI;
 
 public class gamesystem : MonoBehaviour
 {
-    public Text countText;
-    private int count = 0;
-    private float timeleft = 0.05f;
+    private int counter = 0;
+    private float revenue = 0.0f;
+    private float rps = 20.0f;
+    public GameObject revenueText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateRevenue();
     }
 
     // Update is called once per frame
-    void UpdateLabel()
+    void Update()
     {
-        count++;
+        float delta = Time.deltaTime;
+        revenue += rps * delta;
+        if (counter % 60 == 0)
+        {
+            Debug.Log(revenue);
+        }
+
+        
+    }
+    void UpdateRevenue()
+    { 
+        revenueText.GetComponent<Text>().text = revenue.ToString("C0");
     }
 }
