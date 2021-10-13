2ac4d03fd01f620f9bdcf2f6eb290052a2dc84b0
林健人 <ion02181128@gmail.com>
2021-10-13 18:17:05 +0900

    8回目

---
+++ Assets/Scenes/ProductSerect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanager2 : MonoBehaviour
+{
+    [SerializeField] GameObject BPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void ClickHamButtonBHamButtonSHamButton()
+    {
+        BPanel.SetActive(true);
+    }
+
+
+
+    public void ClickYesButtonNoButton()
+    {
+        BPanel.SetActive(false);
+    }
+}
