9b08ce2c4a1762629d42f1a98e907e2a5faeb5b8
林健人 <ion02181128@gmail.com>
2021-11-03 13:55:07 +0900

    13失敗

---
+++ Assets/Scenes/Ham.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Ham : MonoBehaviour
+{
+    public int lebel = 0;
+    public static decimal revenue = 3000000.0m;
+    public static decimal rps = 20.0m;
+    private int count;
+    [SerializeField] GameObject YesButton;
+    // Start is called before the first frame update
+    public void ClickYesButton()
+    {
+        count += 1;
+        while (count <=10)
+        {
+            lebel += 1;
+            revenue -= 200000 * count;
+            rps += 40.0m * count;
+        }
+    }
+
+
+    // Update is called once per frame
+
+}
