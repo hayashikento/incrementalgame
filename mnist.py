c2274f5c75562f3ebab4d31d3cfea4bcf1da59b6
林健人 <ion02181128@gmail.com>
2022-02-23 14:30:00 +0900

    28

--- mnist.py
+++ mnist.py
@@ -3,20 +3,25 @@ from sklearn.linear_model import SGDClassifier
 import numpy as np
 import pickle
 from sklearn.model_selection import cross_val_score
+import json
+import pandas as pd
 
+with open('temp.json') as f:
+    df = json.load(f)
+sf_Features = json.dumps(df['Features'])
+df_f = pd.read_json(sf_Features)
+print(df_f)
+y = df_f['action']
+X = df_f.drop(['action', 'datetime'], axis=1)
+print(X.shape)
+print(y.shape)
 
-save_file = 'mnist.pkl'
-
-with open(save_file, 'rb') as f:
-    mnist = pickle.load(f)
-
-X, y = mnist["data"], mnist["target"]
-
+df_f = df_f.reset_index()
 
 some_digit = X[0]
 
 y = y.astype(np.uint8)
-X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+X_train, X_test, y_train, y_test = X[:200], X[200:], y[:200], y[200:]
 
 sgd_clf = SGDClassifier(random_state=42)
 sgd_clf.fit(X_train, y_train)
